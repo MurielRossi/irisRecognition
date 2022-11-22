96119daa880fcbe1ebcc1413694bafde0fc99e4a
Muriel Rossi <m.rossi67@studenti.unisa.it>
2022-11-22 16:06:21 +0100

    completed the model

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from matplotlib import pyplot
 from pandas.plotting import scatter_matrix
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -22,22 +23,22 @@ dataset = pd.read_csv(url, names=names)
 print(dataset.shape)
 
 # head
-print(dataset.head(20)) #default, the first 5 instances
+print(dataset.head(20))  # default, the first 5 instances
 
 # descriptions
-#count - The number of not-empty values.
-#mean - The average (mean) value.
+# count - The number of not-empty values.
+# mean - The average (mean) value.
 # std - The standard deviation.
-#min - the minimum value.
-#*Percentile meaning: how many of the values are less than the given percentile.
-#max - the maximum value.
+# min - the minimum value.
+# *Percentile meaning: how many of the values are less than the given percentile.
+# max - the maximum value.
 print(dataset.describe())
 
 # class distribution: how much instances x class
 print(dataset.groupby('class').size())
 
 # box and whisker plots
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
 pyplot.show()
 
 # histograms
@@ -50,8 +51,8 @@ pyplot.show()
 
 # Split-out validation dataset
 array = dataset.values
-X =array[:,0:4]
-y =array[:,4]
+X = array[:, 0:4]
+y = array[:, 4]
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
 
 # Spot Check Algorithms
@@ -66,9 +67,19 @@ models.append(('SVM', SVC(gamma='auto')))
 results = []
 names = []
 for name, model in models:
-	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
-	results.append(cv_results)
-	names.append(name)
-	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
 
